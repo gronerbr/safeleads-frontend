55735f34e4727337a34b9853d36ece3152f586b6
phsantiago <pedroh_s_@hotmail.com>
2017-11-13 21:17:39 -0200

    create dir to App component and fix lint

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.scss'
-import Example from '../components/Example/Example'
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import Example from '../../components/Example/Example';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App" ref={1}>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Hello React World</h1>
         </header>
         <Example />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
