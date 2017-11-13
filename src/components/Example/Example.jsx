19fd91308b49175f52201e02c17a14b8d14af29f
Willian Viana <willian.batista.viana@gmail.com>
2017-11-13 19:58:58 -0200

    added redux modules and components examples

---
+++ src/components/Example/Example.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+const Example = () => {
+    return (
+        <div>
+            <p>It's an example of React component</p>
+        </div>
+    );
+}
+
+export default Example;
