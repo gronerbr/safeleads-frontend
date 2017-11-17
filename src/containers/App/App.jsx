a56074a2938c44c3ed838550e3f91fbc9037ff02
Raphael Guastaferro <rapha.guasta@gmail.com>
2017-11-17 16:14:29 -0200

    Add global componentDidCatch boilerplate

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -5,11 +5,32 @@ import style from './App.scss';
 import Routes from '../Routes/Routes';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({ hasError: true });
+    // eslint-disable-next-line
+    console.log('componentDidCatch info ', info);
+    // eslint-disable-next-line
+    console.log('componentDidCatch error ', error);
+    // You can also log the error to an error reporting service (GA or other)
+    // logErrorToMyService(error, info);
+  }
+
   renderTestMessage = () => {
     return 'This project accepts arrow function in classes';
   }
 
   render() {
+    if (this.state.hasError) {
+      console.error('THERE\'S AN ERROR IN APPLICATION, PLEASE FIX IT!');
+    }
+
     return (
       <div className={style.App} ref={1}>
         <header className={style['App-header']}>
