e2ea568d8d99ec88a26ad7c17873c401184a5d65
phsantiago <pedroh_s_@hotmail.com>
2017-11-17 17:57:34 -0200

    fix connected router

--- src/redux/create.js
+++ src/redux/create.js
@@ -2,10 +2,15 @@ import { createStore as _createStore, applyMiddleware } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import createHistory from 'history/createBrowserHistory';
 import reducer from './modules/reducer';
 
+export const history = createHistory();
+
+const router = routerMiddleware(history);
+
 export default function createStore() {
-  const middleware = [thunk, logger, routerMiddleware];
+  const middleware = [thunk, logger, router];
   const finalCreateStore = applyMiddleware(...middleware)(_createStore);
   const store = finalCreateStore(
     reducer,
