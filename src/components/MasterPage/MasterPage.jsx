7f770359000577d774ce3beaaad72b8186be221a
phsantiago <pedroh_s_@hotmail.com>
2018-01-18 04:35:01 +0000

    turn into component, created toggle menu and fix proptypes location

--- src/components/MasterPage/MasterPage.jsx
+++ src/components/MasterPage/MasterPage.jsx
@@ -1,27 +1,36 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import PropTypes from 'prop-types';
 import Grid from 'material-ui/Grid';
 import Header from '../Header/Header';
 import Sidebar from '../Sidebar/Sidebar';
+import styles from './MasterPage.scss';
 
-function MasterPage({ ...props }) {
-  return (
-    <Fragment>
-      <Header />
-      <Grid container spacing={0}>
-        <Grid item xs={10} md={3}>
-          <Sidebar />
+class MasterPage extends Component {
+  static propTypes = {
+    children: PropTypes.element.isRequired,
+  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      isMenuOpen: true,
+    };
+  }
+  render() {
+    return (
+      <Fragment>
+        <Header onToggleMenu={() => this.setState({ isMenuOpen: !this.state.isMenuOpen })} />
+        <Grid container spacing={0}>
+          <Grid className={this.state.isMenuOpen || styles.menuClosed} item xs={10} md={3}>
+            <Sidebar />
+          </Grid>
+          <Grid item xs={this.state.isMenuOpen ? 2 : 12} md={this.state.isMenuOpen ? 9 : 12}>
+            { this.props.children }
+          </Grid>
         </Grid>
-        <Grid item xs={2} md={9}>
-          { props.children }
-        </Grid>
-      </Grid>
-    </Fragment>
-  );
+      </Fragment>
+    );
+  }
 }
 
-MasterPage.propTypes = {
-  children: PropTypes.element.isRequired,
-};
 
 export default MasterPage;
