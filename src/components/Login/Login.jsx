97f471176cb6b23338944cf2f26c0320755d197b
phsantiago <pedroh_s_@hotmail.com>
2018-01-19 16:06:21 +0000

    refactor unused themeprovider

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,6 @@
 import React, { Fragment } from 'react';
 import FontAwesome from 'react-fontawesome';
 import TextField from 'material-ui/TextField';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Button from 'material-ui/Button';
 import Icon from 'material-ui/Icon';
 import { Link } from 'react-router-dom';
@@ -12,45 +11,43 @@ import logo from '../../img/logo.png';
 const Login = () => (
   <div className={styles.bg}>
     <div className={styles.card}>
-      <MuiThemeProvider>
-        <div className={styles.login}>
-          <img src={logo} className={styles.logo} alt="logo safe leads" />
-          <form className={styles.login__form}>
-            <div className={styles.inputWrapper}>
-              <div className={styles.icon}>
-                <Icon className="material-icons">person_outline</Icon>
-              </div>
-              <TextField
-                placeholder="Digite seu login"
-                label="Usuário"
-                type="text"
-                margin="normal"
-              />
-              <br/>
-              <div className={styles.icon}>
-                <Icon className='material-icons'>lock_outline</Icon>
-              </div>
-              <TextField
-                placeholder="Digite sua senha"
-                label="Senha"
-                type="password"
-                margin="normal"
-              />
+      <div className={styles.login}>
+        <img src={logo} className={styles.logo} alt="logo safe leads" />
+        <form className={styles.login__form}>
+          <div className={styles.inputWrapper}>
+            <div className={styles.icon}>
+              <Icon className="material-icons">person_outline</Icon>
             </div>
-            <div className={styles.buttonWrapper}>
-              <Link to="/produtos">
-                <Button
-                  raised
-                  className={styles.button}
-                >
-                  LOGIN
-                </Button>
-              </Link>
+            <TextField
+              placeholder="Digite seu login"
+              label="Usuário"
+              type="text"
+              margin="normal"
+            />
+            <br/>
+            <div className={styles.icon}>
+              <Icon className='material-icons'>lock_outline</Icon>
             </div>
-            <a className={styles.forgotten} href="#">Esqueci minha senha</a>
-          </form>
-        </div>
-      </MuiThemeProvider>
+            <TextField
+              placeholder="Digite sua senha"
+              label="Senha"
+              type="password"
+              margin="normal"
+            />
+          </div>
+          <div className={styles.buttonWrapper}>
+            <Link to="/produtos">
+              <Button
+                raised
+                className={styles.button}
+              >
+                LOGIN
+              </Button>
+            </Link>
+          </div>
+          <a className={styles.forgotten} href="#">Esqueci minha senha</a>
+        </form>
+      </div>
     </div>
   </div>
 );
