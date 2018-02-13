1ba18a6b398ac753dd46fb239f7c97e41434c582
phsantiago <pedroh_s_@hotmail.com>
2018-02-13 06:07:34 +0000

    handful fn to get youtube video thumbnail

---
+++ src/utils/youtubeThumbnail.test.js
@@ -0,0 +1,17 @@
+import youtubeThumbnail from './youtubeThumbnail';
+
+describe("youtubeThumbnail", () => {
+  it("should return false when not found", () => {
+    expect(youtubeThumbnail()).toBe(false);
+  })
+  it("should return the video id when the url is shortner", () => {
+    const shortUrl = "https://youtu.be/lgJOJAmXlBw";
+    expect(youtubeThumbnail(shortUrl)).toBe("https://img.youtube.com/vi/lgJOJAmXlBw/sddefault.jpg");
+  })
+  it("should return the video id when receive a default youtube url", () => {
+    const url = "https://www.youtube.com/watch?v=lgJOJAmXlBw";
+    expect(youtubeThumbnail(url)).toBe("https://img.youtube.com/vi/lgJOJAmXlBw/sddefault.jpg");
+  })
+
+})
+
