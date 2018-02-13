1412e5569d0933aa23202c6865636f6aee8c0979
phsantiago <pedroh_s_@hotmail.com>
2018-02-13 23:51:42 +0000

    fix list to the new mock

--- scripts/mock.js
+++ scripts/mock.js
@@ -9,7 +9,25 @@ var db = {
       "year": "2017/2017",
       "price": 91000.12,
       "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed pretium metus, vel vehicula sem. Praesent vel dolor justo. Ut nulla ante, tincidunt sed diam semper, venenatis fringilla odio. Quisque a arcu id lorem tincidunt molestie. Nam ut massa ut libero viverra dictum. Vivamus at facilisis urna, sit amet congue tellus. Nullam sodales et justo non tincidunt",
-      "img": "http://s2.glbimg.com/Qh4HLCZ7DBbwwk7qR9Ch8RWquAU=/s.glbimg.com/jo/g1/f/original/2016/09/20/ford-fusion-111.jpg"
+      "images": [
+        {
+          "src": "http://s2.glbimg.com/Qh4HLCZ7DBbwwk7qR9Ch8RWquAU=/s.glbimg.com/jo/g1/f/original/2016/09/20/ford-fusion-111.jpg",
+          "title": "lorem ipsum dolor sit ammet",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ],
+      "videos": [
+        {
+          "youtube": "https://www.youtube.com/watch?v=5RWS-Kq1kgs",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        },
+        {
+          "youtube": "https://www.youtube.com/watch?v=5f_jKEmlkTU",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ]
     },
     {
       "id": 2,
@@ -17,7 +35,25 @@ var db = {
       "year": "2017/2018",
       "price": 80000.00,
       "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed pretium metus, vel vehicula sem. Praesent vel dolor justo. Ut nulla ante, tincidunt sed diam semper, venenatis fringilla odio. Quisque a arcu id lorem tincidunt molestie. Nam ut massa ut libero viverra dictum. Vivamus at facilisis urna, sit amet congue tellus. Nullam sodales et justo non tincidunt",
-      "img": "https://i.ytimg.com/vi/zD11h10aHjI/maxresdefault.jpg"
+      "images": [
+        {
+          "src": "https://1.bp.blogspot.com/-isC8efsuPzI/WFsBTfWML9I/AAAAAAACgKI/WwVPk9_3ThkWYzGDMzIEsxX8pvIjEVKZQCLcB/s1600/Toyota-Corolla-Furia-2018%2B%25283%2529.jpg",
+          "title": "lorem ipsum dolor sit ammet",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ],
+      "videos": [
+        {
+          "youtube": "https://www.youtube.com/watch?v=5RWS-Kq1kgs",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        },
+        {
+          "youtube": "https://www.youtube.com/watch?v=5f_jKEmlkTU",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ]
     },
     {
       "id": 3,
@@ -25,7 +61,25 @@ var db = {
       "year": "2017/2017",
       "price": 91000.12,
       "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed pretium metus, vel vehicula sem. Praesent vel dolor justo. Ut nulla ante, tincidunt sed diam semper, venenatis fringilla odio. Quisque a arcu id lorem tincidunt molestie. Nam ut massa ut libero viverra dictum. Vivamus at facilisis urna, sit amet congue tellus. Nullam sodales et justo non tincidunt",
-      "img": "http://s2.glbimg.com/Qh4HLCZ7DBbwwk7qR9Ch8RWquAU=/s.glbimg.com/jo/g1/f/original/2016/09/20/ford-fusion-111.jpg"
+      "images": [
+        {
+          "src": "http://s2.glbimg.com/Qh4HLCZ7DBbwwk7qR9Ch8RWquAU=/s.glbimg.com/jo/g1/f/original/2016/09/20/ford-fusion-111.jpg",
+          "title": "lorem ipsum dolor sit ammet",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ],
+      "videos": [
+        {
+          "youtube": "https://www.youtube.com/watch?v=5RWS-Kq1kgs",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        },
+        {
+          "youtube": "https://www.youtube.com/watch?v=5f_jKEmlkTU",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ]
     },
     {
       "id": 4,
@@ -33,7 +87,25 @@ var db = {
       "year": "2017/2018",
       "price": 80000.00,
       "desc": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur sed pretium metus, vel vehicula sem. Praesent vel dolor justo. Ut nulla ante, tincidunt sed diam semper, venenatis fringilla odio. Quisque a arcu id lorem tincidunt molestie. Nam ut massa ut libero viverra dictum. Vivamus at facilisis urna, sit amet congue tellus. Nullam sodales et justo non tincidunt",
-      "img": "https://i.ytimg.com/vi/zD11h10aHjI/maxresdefault.jpg"
+      "images": [
+        {
+          "src": "https://precoscarros.com.br/wp-content/uploads/2017/10/Novo-Corolla-2019-02.jpg",
+          "title": "lorem ipsum dolor sit ammet",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ],
+      "videos": [
+        {
+          "youtube": "https://www.youtube.com/watch?v=5RWS-Kq1kgs",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        },
+        {
+          "youtube": "https://www.youtube.com/watch?v=5f_jKEmlkTU",
+          "title": "adasdas",
+          "desc": "lorem ipsum dolor sit ammet"
+        }
+      ]
     }
   ]
 }
