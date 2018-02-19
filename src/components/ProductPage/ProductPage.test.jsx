e4cc26951dbf159af2413b36cee33597d1bfa177
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-02-18 22:11:10 -0300

    [product HOC] fix tests

--- src/components/ProductPage/ProductPage.test.jsx
+++ src/components/ProductPage/ProductPage.test.jsx
@@ -1,37 +1,43 @@
 import React from 'react';
-import ProductPage from './ProductPage.jsx';
 import { shallow } from 'enzyme';
+import ProductPage from './ProductPage';
 
 describe('ProductPage', () => {
   it('shoud mount', () => {
-    const component = shallow(<ProductPage />);
-  })
+    const component = shallow(<ProductPage product={{}} />); //eslint-disable-line
+  });
   describe('media videos', () => {
-    const resultContainer = jest.fn()
-    const component = shallow(<ProductPage updateProduct={patch => resultContainer(patch) } />);
+    const resultContainer = jest.fn();
+    const component = shallow(<ProductPage
+      product={{}}
+      videos={[
+        {
+          thumbnail: '',
+        },
+      ]}
+      updateProduct={patch => resultContainer(patch)}
+    />);
 
     it('should add video thumbnail when user input youtube url', () => {
-
       component.instance().handleVideoChange({
-        target: { value: 'https://www.youtube.com/watch?v=m9VA8_lLiWw' }
+        target: { value: 'https://www.youtube.com/watch?v=m9VA8_lLiWw' },
       }, 0, 'youtube');
 
       const firstParameterOfFirstCall = resultContainer.mock.calls[0][0];
 
       expect(firstParameterOfFirstCall.videos[0].thumbnail).toBeDefined();
       expect(firstParameterOfFirstCall.videos[0].thumbnail.length).toBeGreaterThan(1);
-    })
+    });
 
     it('should remove video thumbnail when user input a non youtube url', () => {
-
       component.instance().handleVideoChange({
-        target: { value: 'https://www.youtube.com/' }
+        target: { value: 'https://www.youtube.com/' },
       }, 0, 'youtube');
 
       const firstParameterOfSecondCall = resultContainer.mock.calls[1][0];
 
       expect(firstParameterOfSecondCall.videos[0].thumbnail).toBeDefined();
       expect(firstParameterOfSecondCall.videos[0].thumbnail.length).toBe(0);
-    })
-  })
-})
+    });
+  });
+});
