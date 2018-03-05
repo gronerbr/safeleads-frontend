1a7e1ca70c6e79e3200d54322c02f6d5e0c80192
Pedro Henrique Santiago <pedroh_s_@hotmail.com>
2018-03-04 23:07:08 -0300

    [product ads] wip

---
+++ src/components/ProductPage/ListAds.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './ProductPage.scss';
+
+function ListAds() {
+  return (
+    <div className={styles.base} />
+  );
+}
+
+ListAds.defaultProps = {};
+
+ListAds.propTypes = {};
+
+export default ListAds;
